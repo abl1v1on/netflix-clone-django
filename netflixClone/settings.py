ea1eab1e70439b4d7fd2e591eea5b9ec4211a587
abl1v <abl1v1on@yandex.ru>
2024-03-27 21:47:45 +0500

    add docker and readme.md

--- netflixClone/settings.py
+++ netflixClone/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-sgp=fz+x&@)wl2n=%+n_i(xi98w)#cyaevk&512gezzrs(#ga4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
 
 
 # Application definition
